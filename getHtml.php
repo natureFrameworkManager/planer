58293391ce8355f82d772352daa97fc0d65c9211
Caspar Kroll <ckroll@schulzentrum.de>
2026-02-11 16:37:34 +0100

    Updated plan url

--- getHtml.php
+++ getHtml.php
@@ -1,6 +1,6 @@
 <?php
 $curl = curl_init();
-curl_setopt($curl, CURLOPT_URL, "https://www.informatik.uni-leipzig.de/~stundenplan/modul.html");//"https://www.informatik.uni-leipzig.de/~stundenplan/archiv/24w/modul.html");
+curl_setopt($curl, CURLOPT_URL, "https://stdplan.informatik.uni-leipzig.de/modul.html");//"https://www.informatik.uni-leipzig.de/~stundenplan/archiv/24w/modul.html");
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 
 $response_data = curl_exec($curl);
