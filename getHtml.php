940e568badd272d4f8e2530ac4405f205833dcd1
laslo <laslo.hauschild@web.de>
2026-03-24 16:27:06 +0100

    create python project

--- getHtml.php
+++
@@ -1,9 +0,0 @@
-<?php
-$curl = curl_init();
-curl_setopt($curl, CURLOPT_URL, "https://stdplan.informatik.uni-leipzig.de/modul.html");//"https://www.informatik.uni-leipzig.de/~stundenplan/archiv/24w/modul.html");
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-
-$response_data = curl_exec($curl);
-curl_close($curl);
-echo $response_data;
-?>
