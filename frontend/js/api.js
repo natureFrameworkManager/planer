cfe185bc62e11e16de081372c1be587919dbc89c
Caspar Kroll <mail@casparkroll.de>
2026-03-29 22:43:54 +0200

    Added JSDoc to api

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -6,34 +6,111 @@ async function apiFetch(path) {
     return res.json();
 }
 
+/**
+ * @typedef {Object} ModuleSimple
+ * @property {number} id
+ * @property {string} name
+ * @property {string} module_number
+ * @property {number} credits
+ * @property {string} planung
+ * @property {string} language
+ */
+/**
+ * @typedef {Object} Module
+ * @property {number} id
+ * @property {string} name
+ * @property {string} module_number
+ * @property {number} credits
+ * @property {string} planung
+ * @property {string} language
+ * @property {number[]} degrees
+ * @property {number[]} events
+ */
+/**
+ * @typedef {Object} Degree
+ * @property {number} id
+ * @property {string} name
+ * @property {number[]} semesters
+ * @property {number[]} module_ids
+ */
+/**
+ * @typedef {Object} DegreeDetail
+ * @property {number} id
+ * @property {string} name
+ * @property {number[]} semesters
+ * @property {ModuleSimple[]} modules
+ */
+/**
+ * @typedef {Object} Event
+ * @property {number} id
+ * @property {string} type
+ * @property {string} title
+ * @property {number} weekday
+ * @property {string} start_time
+ * @property {string} end_time
+ * @property {number} location_id
+ * @property {string} status
+ * @property {number[]} module_ids
+ * @property {number[]} staff_ids
+ */
+
+/**
+ * 
+ * @returns {Promise<Degree[]>}
+ */
 export async function fetchDegrees() {
     return apiFetch(
         "/degrees?include_relationships=true&include_semesters=true",
     );
 }
 
+/**
+ * 
+ * @param {number} id 
+ * @returns {Promise<DegreeDetail>}
+ */
 export async function fetchDegreeDetail(id) {
     return apiFetch(
         `/degrees/${id}?include_relationships=true&include_semesters=true`,
     );
 }
 
+/**
+ * 
+ * @returns {Promise<Module[]>}
+ */
 export async function fetchModules() {
     return apiFetch("/modules?include_relationships=true");
 }
 
+/**
+ * 
+ * @returns {Promise<Event[]>}
+ */
 export async function fetchEvents() {
     return apiFetch("/events?include_relationships=true");
 }
 
+/**
+ * 
+ * @returns {Promise<{id: number, name: string}[]>}
+ */
 export async function fetchStaff() {
     return apiFetch("/staff");
 }
 
+/**
+ * 
+ * @returns {Promise<{id: number, name: string}[]>}
+ */
 export async function fetchLocations() {
     return apiFetch("/locations");
 }
 
+/**
+ * 
+ * @returns {Promise<{id: number, name: string}[]>}
+ */
 export async function fetchSemesters() {
     return apiFetch("/semesters");
 }
